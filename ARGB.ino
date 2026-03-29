75a5ef9237e889033239ec8e26fac1558b6be104
V-isaac <79458239+V-isaac@users.noreply.github.com>
2026-03-29 21:36:59 +0300

    feat: new button and some refactoring

--- ARGB.ino
+++ ARGB.ino
@@ -2,8 +2,9 @@
 #include <FastLED.h>
 
 #define DATA_PIN    5
-#define INC_MODE    8
+#define INC_MODE    10
 #define INC_COL     9
+#define INC_VAL     8
 
 #define LED_S       8
 #define BRIGHTNESS  255
@@ -15,42 +16,27 @@
 // Define the array of leds
 CRGB leds[LED_S];
 
-struct col{
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-};
+struct ReGrBl{ uint8_t r, g, b; };
 
-struct HSV{
-  float h;
-  float s;
-  float v;
-};
+struct HuSaVa{ float h, s, v; };
 
-col C = {0xFF, 0x00, 0x00};
-col inside = C;
-HSV Chsv = {0, 1, 1}; // standard red HSV
+struct Colour;
 
-col gradient[LED_S] = {
-{0xFF, 0x00, 0x00},
-{0xDF, 0x00, 0x00},
-{0xCF, 0x00, 0x00},
-{0xAF, 0x00, 0x00},
-{0x8F, 0x00, 0x00},
-{0x4F, 0x00, 0x00},
-{0x2F, 0x00, 0x00},
-{0x00, 0x00, 0x00},
-};
+void RGBtoHSV(Colour* c);
+void HSVtoRGB(Colour* c);
 
-HSV hgrad[LED_S] = {
-{0, 1, 1},
-{0, 1, .9},
-{0, 1, .7},
-{0, 1, .5},
-{0, 1, .3},
-{0, 1, .1},
-{0, 1, .0},
-{0, 1, .0},
+struct Colour{
+  ReGrBl ColRGB;
+  HuSaVa ColHSV;
+
+  void SetRGB(const Colour& c){
+    ColRGB = c.ColRGB;
+    RGBtoHSV(this);
+  }
+  void SetHSV(const Colour& c){
+    ColHSV = c.ColHSV;
+    HSVtoRGB(this);
+  }
 };
 
 static inline float minimum(float a, float b){
@@ -71,80 +57,90 @@ static inline float maximum3(float a, float b, float c){
   return maximum(a, maximum(b,c));
 }
 
-HSV RGBtoHSV(col colour){
-  float r = colour.r / 255.f;
-  float g = colour.g / 255.f;
-  float b = colour.b / 255.f;
+void RGBtoHSV(Colour* col){
+  float r = col->ColRGB.r / 255.f;
+  float g = col->ColRGB.g / 255.f;
+  float b = col->ColRGB.b / 255.f;
 
   float cmax = maximum3(r,g,b);
   float cmin = minimum3(r,b,b);
   float delta = cmax - cmin;
 
-  HSV ret;
-
   if(delta < 1e-6f){
-    ret.h = 0.f;
+    col->ColHSV.h = 0.f;
   } else if (cmax == r){
-    ret.h = 60.f * fmodf(((g - b) / delta), 6.f);
+    col->ColHSV.h = 60.f * fmodf(((g - b) / delta), 6.f);
   } else if (cmax == g){
-    ret.h = 60.f * (((b - r) / delta) + 2.f);
+    col->ColHSV.h = 60.f * (((b - r) / delta) + 2.f);
   } else{
-    ret.h = 60.f * (((r - g) / delta) + 4.f);
+    col->ColHSV.h = 60.f * (((r - g) / delta) + 4.f);
   }
 
-  if(ret.h < 0.f) ret.h += 360.f;
+  // return
+  if(col->ColHSV.h < 0.f) col->ColHSV.h += 360.f;
+  col->ColHSV.s = (cmax <= 0.f) ? 0.f : (delta/cmax);
+  col->ColHSV.v = cmax;
 
-  ret.s = (cmax <= 0.f) ? 0.f : (delta/cmax);
-
-  ret.v = cmax;
-
-  if(ret.h < 0.f) ret.h = 0.f;
-  if(ret.h > 360.f) ret.h = 360.f;
-  if(ret.s < 0.f) ret.h = 0.f;
-  if(ret.s > 1.f) ret.h = 1.f;
-  if(ret.v < 0.f) ret.h = 0.f;
-  if(ret.v > 1.f) ret.h = 1.f;
-
-  return ret;
+  // clamp 
+  if(col->ColHSV.h < 0.f) col->ColHSV.h = 0.f;
+  if(col->ColHSV.h > 360.f) col->ColHSV.h = 360.f;
+  if(col->ColHSV.s < 0.f) col->ColHSV.h = 0.f;
+  if(col->ColHSV.s > 1.f) col->ColHSV.h = 1.f;
+  if(col->ColHSV.v < 0.f) col->ColHSV.h = 0.f;
+  if(col->ColHSV.v > 1.f) col->ColHSV.h = 1.f;
 }
 
-col HSVtoRGB(HSV col){
-  col.h = fmodf(col.h, 360.0f);
-  if(col.h < 0) col.h += 360.0f;
-  if(col.s < 0) col.s = 0; else if (col.s > 1) col.s = 1;
-  if(col.v < 0) col.v = 0; else if (col.v > 1) col.v = 1;
+void HSVtoRGB(Colour* col){
+  col->ColHSV.h = fmodf(col->ColHSV.h, 360.0f);
+  if(col->ColHSV.h < 0) col->ColHSV.h += 360.0f;
+  if(col->ColHSV.s < 0) col->ColHSV.s = 0;
+    else if (col->ColHSV.s > 1) col->ColHSV.s = 1;
+  if(col->ColHSV.v < 0) col->ColHSV.v = 0; 
+    else if (col->ColHSV.v > 1) col->ColHSV.v = 1;
 
-  float c = col.v * col.s;
-  float x = c *(1.0f - fabsf(fmodf(col.h / 60.0f, 2.0f) - 1.0f));
-  float m = col.v - c;
+  float c = col->ColHSV.v * col->ColHSV.s;
+  float x = c *(1.0f - fabsf(fmodf(col->ColHSV.h / 60.0f, 2.0f) - 1.0f));
+  float m = col->ColHSV.v - c;
 
   float r1, g1, b1;
-  if (col.h < 60.0f) {
+  if (col->ColHSV.h < 60.0f) {
     r1 = c; g1 = x; b1 = 0;
-  } else if (col.h < 120.0f) {
+  } else if (col->ColHSV.h < 120.0f) {
     r1 = x; g1 = c; b1 = 0;    
-  } else if (col.h < 180.0f) {
+  } else if (col->ColHSV.h < 180.0f) {
     r1 = 0; g1 = c; b1 = x;    
-  } else if (col.h < 240.0f) {
+  } else if (col->ColHSV.h < 240.0f) {
     r1 = 0; g1 = x; b1 = c;    
-  } else if (col.h < 300.0f) {
+  } else if (col->ColHSV.h < 300.0f) {
     r1 = x; g1 = 0; b1 = c;    
   } else {
     r1 = c; g1 = 0; b1 = x;    
   }
 
-  return {
-    (uint8_t)roundf((r1 + m) * 255.0f),
-    (uint8_t)roundf((g1 + m) * 255.0f),
-    (uint8_t)roundf((b1 + m) * 255.0f)    
-  };
+  // return
+  col->ColRGB.r = (uint8_t)roundf((r1 + m) * 255.0f);
+  col->ColRGB.g = (uint8_t)roundf((g1 + m) * 255.0f);
+  col->ColRGB.b = (uint8_t)roundf((b1 + m) * 255.0f);   
 }
 
 
-void Mult(col* color, float mult){
-  color->r = color->r * mult;
-  color->g = color->g * mult;
-  color->b = color->b * mult;
+//Colour C = {0xF0, 0x00, 0xFF};
+Colour C = {0xFF, 0x00, 0x00};
+Colour inside = C;
+
+Colour gradient[LED_S] = {
+    {{0xF0, 0x00, 0xFF}, {}},
+    {{0xD0, 0x00, 0xDF}, {}},
+    {{0xC0, 0x00, 0xCF}, {}},
+    {{0xA0, 0x00, 0xAF}, {}},
+    {{0x80, 0x00, 0x8F}, {}},
+    {{0x40, 0x00, 0x4F}, {}},
+    {{0x20, 0x00, 0x2F}, {}},
+    {{0x00, 0x00, 0x00}, {}},
+};
+
+void Mult(Colour* color, float mult){
+  color->ColHSV.v = color->ColHSV.v * mult;
 }
 
 inline bool CheckMode(){
@@ -163,10 +159,10 @@ int wrap(int kx) {
 
 int offset = 0;
 
-inline void ScanOne(col colour){
+inline void ScanOne(Colour c){
   for(int i = 0; i < LED_S; ++i){
     if(i == wrap(offset))
-      leds[i].setRGB(colour.r, colour.g, colour.b);
+      leds[i].setRGB(c.ColRGB.r, c.ColRGB.g, c.ColRGB.b);
     else
       leds[i].setRGB(0,0,0);
   }
@@ -177,19 +173,19 @@ inline void ScanOne(col colour){
 
 float fade = 0;
 bool isUp = 1;
-inline void FadeInOut(col Color, float fade){
-  HSV hsv = RGBtoHSV(Color);
-  hsv.v -= fade;
-  Color = HSVtoRGB(hsv);
 
-  Constant(Color);
+inline void FadeInOut(Colour c, float fade){
+  c.ColHSV.v -= fade;
+  c.SetHSV(c);
+
+  Constant(c);
   FastLED.show();
 }
 
-inline void ScanTwo(col Color){
+inline void ScanTwo(Colour c){
   for(int i = 0; i < LED_S; ++i){
     if(i == wrap(offset) || i == wrap(offset + LED_S/2))
-      leds[i].setRGB(Color.r, Color.g, Color.b);
+      leds[i].setRGB(c.ColRGB.r, c.ColRGB.g, c.ColRGB.b);
     else
       leds[i].setRGB(0,0,0);
   }
@@ -201,33 +197,50 @@ inline void ScanTwo(col Color){
 inline void ScanGrad(){
   for(int i = 0; i < LED_S; ++i){
       leds[i] = (CRGB){
-      gradient[wrap(i + offset)].r, 
-      gradient[wrap(i + offset)].g,
-      gradient[wrap(i + offset)].b};
+      gradient[wrap(i + offset)].ColRGB.r, 
+      gradient[wrap(i + offset)].ColRGB.g,
+      gradient[wrap(i + offset)].ColRGB.b};
   }
 
   offset++;
   if(offset >= LED_S) offset = 0;
 }
 
-inline void Constant(col Color){
+inline void Constant(Colour c){
   for(int i = 0; i < LED_S; ++i){
-    leds[i] = (CRGB){Color.r, Color.g, Color.b};
+    leds[i] = (CRGB){c.ColRGB.r, c.ColRGB.g, c.ColRGB.b};
   }
 }
 
 uint8_t mode = 0;
 
 void setup() {
-  Mult(&inside, 0.25);
-
   Serial.begin(115200);
+  Mult(&inside, 0.25);
+  inside.SetHSV(inside);
+
+  C.SetRGB(C);
+  for(int i = 0; i < LED_S; ++i){
+    gradient[i].SetRGB(gradient[i]);
+  }
+
+  Serial.println("initial color");
+  Serial.println("r\tg\tb\th\ts\tv");
+  Serial.print(C.ColRGB.r); Serial.print('\t');
+  Serial.print(C.ColRGB.g); Serial.print('\t');
+  Serial.print(C.ColRGB.b); Serial.print('\t');
+  Serial.print(C.ColHSV.h); Serial.print('\t');
+  Serial.print(C.ColHSV.s); Serial.print('\t');
+  Serial.print(C.ColHSV.v); Serial.println();
+
+
   pinMode(INC_MODE, INPUT);
   pinMode(INC_COL, INPUT);
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, LED_S);
   FastLED.setBrightness(BRIGHTNESS);
 }
 
+int valUp = 0;
 
 void loop() {
   if(digitalRead(INC_MODE)){
@@ -241,25 +254,59 @@ void loop() {
     delay(100);
     Serial.println("changed color");
     Serial.println("r\tg\tb\th\ts\tv");
-    Chsv.h += 10;
-    C = HSVtoRGB(Chsv);
-    Serial.print(C.r); Serial.print('\t');
-    Serial.print(C.g); Serial.print('\t');
-    Serial.print(C.b); Serial.print('\t');
-    Serial.print(Chsv.h); Serial.print('\t');
-    Serial.print(Chsv.s); Serial.print('\t');
-    Serial.print(Chsv.v); Serial.println();
+    C.ColHSV.h += 10;
+    C.SetHSV(C);
+    Serial.print(C.ColRGB.r); Serial.print('\t');
+    Serial.print(C.ColRGB.g); Serial.print('\t');
+    Serial.print(C.ColRGB.b); Serial.print('\t');
+    Serial.print(C.ColHSV.h); Serial.print('\t');
+    Serial.print(C.ColHSV.s); Serial.print('\t');
+    Serial.print(C.ColHSV.v); Serial.println();
 
     for(int i = 0; i < LED_S; ++i){
-      hgrad[i].h += 10.f;
-      if(hgrad[i].h > 360.f) hgrad[i].h = 10.f;
-      gradient[i] = HSVtoRGB(hgrad[i]);
+      gradient[i].ColHSV.h += 10.f;
+      if(gradient[i].ColHSV.h > 361.f) gradient[i].ColHSV.h = 10.f;
+      gradient[i].SetHSV(gradient[i]);
     }
+  }
+
+  if(digitalRead(INC_VAL)){
+    delay(100);
+    Serial.println("changed value");
+    Serial.println("r\tg\tb\th\ts\tv");
+    if(!valUp){
+      C.ColHSV.v -= .10;
+      if(C.ColHSV.v < 0){
+        C.ColHSV.v = 0;
+        valUp = 1;
+      } 
+    } else{
+      C.ColHSV.v += .10;
+      if(C.ColHSV.v > 1){
+        C.ColHSV.v = 1;
+      }
+    }
+
+    C.SetHSV(C);
+    Serial.print(C.ColRGB.r); Serial.print('\t');
+    Serial.print(C.ColRGB.g); Serial.print('\t');
+    Serial.print(C.ColRGB.b); Serial.print('\t');
+    Serial.print(C.ColHSV.h); Serial.print('\t');
+    Serial.print(C.ColHSV.s); Serial.print('\t');
+    Serial.print(C.ColHSV.v); Serial.println();
+
+    /*
+    for(int i = 0; i < LED_S; ++i){
+      gradient[i].ColHSV.h += 10.f;
+      if(gradient[i].ColHSV.h > 361.f) gradient[i].ColHSV.h = 10.f;
+      gradient[i].SetHSV(gradient[i]);
+    }
+    */
   }
 
   switch(mode){
     case 0:
-      Constant((col){0,0,0});
+      Constant((Colour){{0,0,0},{}});
         FastLED.show();
       break;
     case 1:
@@ -286,7 +333,7 @@ void loop() {
       delay(50);
       break;
     default:
-      Constant((col){0x10, 0x10, 0x10});
+      Constant((Colour){{0x10, 0x10, 0x10}, {}});
       FastLED.show();
       delay(DELAY);
       break;
